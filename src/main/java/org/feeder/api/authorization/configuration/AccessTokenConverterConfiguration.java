3e7505a5357ddf1461fc535cfe809ec032e82502
Andrei Varashen <voroshen.av@gmail.com>
2020-04-21 00:33:59 +0300

    REL1-17: make access token as JWT (#12)
    Co-authored-by: avarashen <avarashen@exadel.com>

---
+++ src/main/java/org/feeder/api/authorization/configuration/AccessTokenConverterConfiguration.java
@@ -0,0 +1,45 @@
+package org.feeder.api.authorization.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@Slf4j
+@Configuration
+public class AccessTokenConverterConfiguration {
+
+  @Configuration
+  @Profile("local")
+  public static class MacAccessTokenConverterConfiguration {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+
+      log.debug("Using MAC based access token signature");
+
+      JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+      converter.setSigningKey("local_signing_key");
+
+      return converter;
+    }
+
+  }
+
+  @Configuration
+  @Profile("!local")
+  public static class RsaAccessTokenConverterConfiguration {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+
+      log.debug("Using RSA based access token signature");
+
+      JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//      converter.setKeyPair(); TODO: add KeyPair generation
+
+      return converter;
+    }
+  }
+}
